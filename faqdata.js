8ee3e88771caee32298ab99255d30de7f6fcce28
louisesorensen-dot <louise.sorensen@visma.com>
2026-04-22 16:00:19 +0200

    Update FAQ question and answer for deployment test

--- faqdata.js
+++ faqdata.js
@@ -1,7 +1,7 @@
 export const data = [
   {
-    q: "TEST: VIRKER DEPLOY?",
-    a: "Hvis du kan se dette på siden, virker din opdatering.",
+    q: "🔥 TEST OM DEPLOY VIRKER",
+    a: "Hvis du ser dette, er alt OK",
     c: "TEST"
   }
 ];
